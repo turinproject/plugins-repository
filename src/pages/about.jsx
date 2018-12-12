bb4a8b80e32c954cf83950459ed6e24408f8752e
William Gladstone <will@jollypebble.com>
2018-12-12 14:22:30 -0500

    restructure plugins

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,14 +4,14 @@ import Layout from "../layout";
 import About from "../components/About";
 import config from "../../data/SiteConfig";
 
-class SubmitPlugin extends Component {
+class AboutPage extends Component {
   render() {
     return (
-      <Layout location={this.props.location} title="Submit Plugin">
+      <Layout location={this.props.location} title="About">
         <div className="submit-plugin-container">
           <Helmet>
-            <title>{`Submit Plugin | ${config.siteTitle}`}</title>
-            <link rel="canonical" href={`${config.siteUrl}/admin/`} />
+            <title>{`About | ${config.siteTitle}`}</title>
+            <link rel="canonical" href={`${config.siteUrl}/about/`} />
           </Helmet>
           <About />
         </div>
@@ -20,4 +20,4 @@ class SubmitPlugin extends Component {
   }
 }
 
-export default SubmitPlugin;
+export default AboutPage;
