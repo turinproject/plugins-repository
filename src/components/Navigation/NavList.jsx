e98071e17b1a9d39921cb2b532195c1a3fb086d4
Igor <igor.merezhko26@gmail.com>
2019-03-15 12:36:44 +0200

    Hamburger-menu: Add hamburger menu items & replace avatar with site logo

--- src/components/Navigation/NavList.jsx
+++ src/components/Navigation/NavList.jsx
@@ -34,6 +34,30 @@ function GetNavList(config) {
     component: Link,
     to: "/about/"
   });
+
+  NavList.push({ divider: true });
+
+  NavList.push({
+    primaryText: "Log In",
+    leftIcon: <FontIcon>exit_to_app</FontIcon>,
+    component: Link,
+    to: "/signin/"
+  });
+
+  NavList.push({
+    primaryText: "Sign Up",
+    leftIcon: <FontIcon>person_add</FontIcon>,
+    component: Link,
+    to: "/signup/"
+  });
+
+  NavList.push({ divider: true });
+
+  NavList.push({
+    primaryText: "Log out",
+    leftIcon: <FontIcon>power_settings_new</FontIcon>
+  });
+
   return NavList;
 }
 export default GetNavList;
