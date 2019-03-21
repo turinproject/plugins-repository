82619a6dab4809beb6e663f6a1a9cba35fa409a1
William Gladstone <will@jollypebble.com>
2019-03-21 13:42:04 -0400

    make annon function

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,12 +3,14 @@ import Page from "../templates/page";
 import config from "../../data/SiteConfig";
 
 class About extends Component {
+
   render() {
     return (
       <Page
         location={this.props.location}
         url={`${config.siteUrl}/about/`}
         title='About'
+        content={()=>{}}
       />
     );
   }
