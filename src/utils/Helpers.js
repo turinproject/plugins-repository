ce62a185400f37344732a7404158353fa62fba6e
Igor Merezhko <igor.merezhko26@gmail.com>
2019-04-26 12:15:20 +0300

    Remove unnecessary console

--- src/utils/Helpers.js
+++ src/utils/Helpers.js
@@ -2,11 +2,9 @@ import config from 'data/SiteConfig';
 
 export default class Helpers {
   static getCategoryName(categoryId) {
-    console.log('categoryId', typeof(categoryId));
     if (categoryId === 'undefined') {
       return 'None';
     }
-    console.log('as asdfasdfasdf', categoryId);
     const categories = config.categories[0].options;
     return categories.filter(category => category.id === categoryId)[0].name;
   }
